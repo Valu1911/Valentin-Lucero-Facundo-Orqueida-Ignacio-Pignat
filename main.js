5fc680d9739d861f3bed2678e757499ef58d1209
Valu1911 <valenlucero406@gmail.com>
2026-04-28 22:28:03 -0300

    primer contador

--- main.js
+++ main.js
@@ -1,44 +1,37 @@
-//alert("Valen, Nacho, Fafa");
-//const dni = "48.371.946";
-//let edad = 18;
-//edad = 19;
-//console.log(edad);
-//alert("hola" + " " + usuario);
+let contador = 0;
 
-let DNI = prompt("Ingrese su DNI (sin puntos ni guiones)");
-while (DNI.length < 8 || DNI.length > 8) {
-  alert("DNI no válido, por favor ingrese un DNI de 8 dígitos");
-  DNI = prompt("Ingrese su DNI (sin puntos ni guiones)");
-}
+let incrementButton = document.getElementById("increment");
+let decrementButton = document.getElementById("decrement");
+let counterDisplay = document.getElementById("counter");
 
-let usuario = prompt("Ingrese su nombre");
-while (usuario.length < 3) {
-  alert(
-    "Nombre no válido, por favor ingrese un nombre de al menos 3 caracteres",
-  );
-  usuario = prompt("Ingrese su nombre");
-}
+incrementButton.addEventListener("click", function () {
+  if (contador < 10) {
+    contador++;
+    counterDisplay.textContent = contador;
+  }
+});
 
-let apellido = prompt("Ingrese su apellido");
-while (apellido.length < 3) {
-  alert(
-    "Apellido no válido, por favor ingrese un apellido de al menos 3 caracteres",
-  );
-  apellido = prompt("Ingrese su apellido");
-}
+decrementButton.addEventListener("click", function () {
+  if (contador > 0) {
+    contador--;
+    counterDisplay.textContent = contador;
+  }
+});
+let incrementar2 = document.getElementById("increment2");
+let decrementar2 = document.getElementById("decrement2");
+let contador2 = 0;
+let counterDisplay2 = document.getElementById("counter2");
 
-let mail = prompt("Ingrese su correo electrónico");
-while (mail.length < 5 || !mail.includes("@")) {
-  alert(
-    "Correo electrónico no válido, por favor ingrese un correo electrónico válido",
-  );
-  mail = prompt("Ingrese su correo electrónico");
-}
+incrementar2.addEventListener("click", function () {
+  if (contador2 < 10) {
+    contador2++;
+    counterDisplay2.textContent = contador2;
+  }
+});
 
-let edad = prompt(usuario + " " + "ingrese su edad");
-while (isNaN(edad) || edad < 18 || edad > 120) {
-  alert("Edad no válida, por favor ingrese una edad entre 18 y 120");
-  edad = prompt(usuario + " " + "ingrese su edad real");
-}
-
-alert("Bienvenido " + usuario + " " + apellido);
+decrementar2.addEventListener("click", function () {
+  if (contador2 > 0) {
+    contador2--;
+    counterDisplay2.textContent = contador2;
+  }
+});
